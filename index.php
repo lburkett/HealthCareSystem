c15412c859927b2fa608be127ad85b6d7d85b762
spencerbyw <spencer.bywater@gmail.com>
2015-03-24 22:22:06 -0700

    Basic CSS and Landing Page
    Put together a landing page, with some base styles to be used. The
    landing page is unique compared to the other pages, so I will try and
    get a different page ready for the patient form and such.

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Efferent Patient Care System</title>
+  <meta name="description" content="Efferent Patient Care System">
+  <meta name="author" content="Spencer Bywater, Lowell Burkett, Nikita Bhambhani, Connor Maddox, Kevin Van">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="http://fonts.googleapis.com/css?family=Roboto+Slab:300,700|Source+Sans+Pro:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Favicon 
+  ––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- HEADING-LANDING section -->
+  <div class="section heading-landing">
+    <div class="container">
+      <h1>Efferent Patient Care System</h1>
+    </div>
+  </div>
+
+  <!-- WELCOME section -->
+  <div class="section welcome">
+    <div class="container">
+      <h4 class="wlcm">Welcome!</h4>
+    </div>
+  </div>
+
+  <!-- DIRECT section -->
+  <div class="section direct">
+    <div class="container">
+      <div class="row">
+        <div class="four columns"><br></div>
+        <div class="four columns navigate">
+          <h5>I'm a...</h5>
+          <a href="#" class="button button-primary">Patient</a>
+          <br>
+          <a href="#" class="button button-primary">Doctor</a>
+
+        </div>
+        <div class="four columns"><br></div>
+      </div>
+    </div>
+  </div>
+
+  <!-- FOOTER section -->
+  <div class="footer">
+    <div class="container">
+      <p>&copy; <?php echo date("Y"); ?> Spencer Bywater, Lowell Burkett, Nikita Bhambhani, Connor Maddox, &amp; Kevin Van of Arizona State University.</p>
+    </div>
+  </div>
+  <div class="footerbar color1">
+    <div class="container color1">
+      <div class="row">
+        <div class="color2 color-sty"></div>
+        <div class="color3 color-sty"></div>
+        <div class="color4 color-sty"></div>
+        <div class="color5 color-sty"></div>
+      </div>
+    </div>
+  </div>
+  
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
