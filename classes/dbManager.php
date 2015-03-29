171e6777a11cc12732444f0f2d3a00271b43367c
lburkett <laburket@asu.edu>
2015-03-29 13:13:25 -0700

    Fix formatting in dbManager.php
    Made the spacing and formatting in dbManager.php more consistent.

--- classes/dbManager.php
+++ classes/dbManager.php
@@ -1,55 +1,41 @@
 <?php
-    require_once 'patient.php';
-    require_once   '../data/common.php'; 
+	require_once 'patient.php';
+  require_once '../data/common.php'; 
 
-    class DatabaseManager {
-		/* Initialize new database file */
+  class DatabaseManager {
+    //create new database
+    protected $db; 
         
-        //create new database
-         protected $db; 
-        
-        
-  
-        
-		//constructor
-        public function __construct(){
-            $this->db =  getPDO();
-          
-            
-        }
-        
-        /* Receives Patient object then inserts it into the database */
-		public function addPatient($newPatient) {
-                
-               
-               $this->db->exec("INSERT INTO patient(fname, lname, phone, filledOutBy, 
-                                                doctorRequested, pain, nausea, 
-                                                depression, anxiety, drowsiness, 
-                                                average, resolved)
-                                               VALUES (newPatient->getFirstName(), 
-                                               newPatient->getLastName(), 
-                                               newPatient->getPhoneNumber(),
-                                               newPatient->getFilled(), 
-                                               newPatient->getDoctor(), newPatient->getPain(), 
-                                               newPatient->getNausea(), 
-                                               newPatient->getDepression(), 
-                                               newPatient->getAnxiety(), 
-                                               newPatient->getDrowsiness(), 
-                                               newPatient->getAverage(),0); ");
+    /* Initialize new database file */
+    public function __construct(){
+      $this->db =  getPDO();
+    }
+    /* Receives Patient object then inserts it into the database */
+    public function addPatient($newPatient) {
+      $this->db->exec("INSERT INTO patient(fname, lname, phone, filledOutBy, 
+                                            doctorRequested, pain, nausea, 
+                                            depression, anxiety, drowsiness, 
+                                            average, resolved)
+                                            VALUES (newPatient->getFirstName(), 
+                                            newPatient->getLastName(), 
+                                            newPatient->getPhoneNumber(),
+                                            newPatient->getFilled(), 
+                                            newPatient->getDoctor(), newPatient->getPain(), 
+                                            newPatient->getNausea(), 
+                                            newPatient->getDepression(), 
+                                            newPatient->getAnxiety(), 
+                                            newPatient->getDrowsiness(), 
+                                            newPatient->getAverage(), 0);");
 		}
 		/* Queries the database for a specific doctor name and returns all patient information */
 		public function retrievePatient() {
-			
 			$patient = $this->db->query(
-				'SELECT *
+        'SELECT *
 				FROM patient'
-				
 			);
 			return $patient;
 		}
 		/* Marks the patient id entered as resolved */
-        
-        //needs data from doctor class i presume
 		public function resolvePatient($id) {
             
 		}
