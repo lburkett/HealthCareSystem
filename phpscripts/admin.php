b3ecdba82bcf69c38f135792f81a00bd1bec9f09
lburkett <laburket@asu.edu>
2015-04-17 12:22:15 -0700

    Initial admin setup
    Main admin functions in place.

---
+++ phpscripts/admin.php
@@ -0,0 +1,21 @@
+<?php
+	require_once 'common.php';
+	require_once '../classes/doctor.php';
+    require_once '../classes/adminManager.php';
+
+	$newDoctor = new Doctor;
+    $manager = new adminManager;
+
+	// Sets new patient information
+	$newDoctor->setName($_POST['name']);
+	$newDoctor->setProfession($_POST['profession']);
+	$newDoctor->setEmail($_POST['email']);
+	$newDoctor->setPassword($_POST['password']);
+    
+    // Inserts a doctor and tests a query
+    $manager->addDoctor($newDoctor);
+    $newDoctor = $manager->retrieveDoctor();
+    
+    // Returns to the adminview
+    redirectAndExit("../formReturn.php");
+?>
