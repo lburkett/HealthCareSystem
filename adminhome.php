889116debbe33733e0bc50037523fe7791dda8f1
kevin <kvan3@asu.edu>
2015-04-17 15:28:30 -0700

    no message

--- adminhome.php
+++ adminhome.php
@@ -1,8 +1,6 @@
 <?php require 'templates/meta.php'; ?>
 <!-- Enter any extra code that should go inside the <head> tag here! Do this ONLY if this page needs a script or something that the other pages do not. -->
-<?php require 'templates/header.php'; 
-
-?>
+<?php require 'templates/header.php';?>
   <!-- Any content should go inside the container where indicated -->
   <div class="section content">
     <div class="container">
