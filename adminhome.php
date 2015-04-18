439986f839e745ed48773584e534cd2d614b1a1c
lburkett <laburket@asu.edu>
2015-04-18 10:39:36 -0700

    Revert "Revert "Slight Revamp of Admin Interface""
    This reverts commit 7fb5dc240346ab88cd2426314a29235eb2715125.

--- adminhome.php
+++ adminhome.php
@@ -23,53 +23,70 @@
   <!-- Any content should go inside the container where indicated -->
   <div class="section content">
     <div class="container">
-    <div class="row">
-          <div class="u-pull-right logout-row"><a href="phpscripts/logout.php" class="button button-primary u-full-width">Logout</a></div>
-      <!-- CONTENT GOES HERE! -->
-       <?php
-          
-
-        while($row = $test->fetch(PDO::FETCH_ASSOC)) {
-    	echo "Name: " . $row['name']  . " Profession: "; 
-        echo $row['profession'] . " Email: ";
-        echo $row['email'];
-		echo "<br>";    
-   		 }
-
-
-        ?>
-
-		
-<form action="phpscripts/checkFormAdmin.php" class="doctor-form" method="post">
-     <div class="explanation">
-			<h4>Add Doctor</h4>
-			<p>Fill out new Doctor's info below:</p>
-			</div>
-			<hr>
-			<div class="row">
-				<div class="six columns">
-					<label for="name">Name</label>
-					<input type="text" class="u-full-width" placeholder="Dr. Bob" id="name" name="name">
+    	<!-- CONTENT GOES HERE! -->
+	    <div class="row">
+	    	<div class="u-pull-right logout-row"><a href="phpscripts/logout.php" class="button button-primary u-full-width">Logout</a></div>
+	    </div>
+	    <h3>Admin</h3>
+	    <p>Listed below are all doctors currently in the database. Use the form below to add a <em>new</em> doctor.</p>
+      	<hr>
+      	<div class="row">
+      		<div class="explanation">
+      			<h4>List of all Current Doctors</h4>
+      			<p>Here is a list of all the doctors currently in the database.</p>
+      		</div>
+      		<table class="u-full-width doctor-list">
+      			<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Profession</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+		        <?php
+			        while($row = $test->fetch(PDO::FETCH_ASSOC)) {
+			        	echo "<tr>";
+			        	echo "<td>" . $row['id'] . "</td>";
+				    	echo "<td>" . $row['name']  . "</td>"; 
+				        echo "<td>" . $row['profession'] . "</td>";
+				        echo "<td>" . $row['email'] . "</td>";
+						echo "</tr>";    
+			   		 }
+		        ?>
+		        </tbody>
+	        </table>
+        </div>
+			<br>
+			<div class="explanation">
+      			<h4>Add a New Doctor</h4>
+      			<p>Fill out and submit the form below to add a new doctor to the roster.</p>
+      		</div>
+			<form action="phpscripts/checkFormAdmin.php" class="doctor-form" method="post">
+				<div class="row">
+					<div class="three columns">
+						<label for="name">Name</label>
+						<input type="text" class="u-full-width" placeholder="Dr. Name" id="name" name="name">
+					</div>
+					<div class="three columns">
+						<label for="profession">Profession</label>
+						<input type="text" class="u-full-width" placeholder="Profession" id="profession" name="profession">
+					</div>
+				
+					<div class="three columns">
+						<label for="email">Email</label>
+						<input type="text" class="u-full-width" placeholder="you@example.com" id="email" name="email">
+					</div>
+					<div class="three columns">
+						<label for="password">Password</label>
+						<input type="password" class="u-full-width" placeholder="Password" id="password" name="password">
+					</div>
 				</div>
-				<div class="six columns">
-					<label for="profession">Profession</label>
-					<input type="text" class="u-full-width" placeholder="Profession" id="profession" name="profession">
-				</div>
-			</div>
-			<div class="row">
-				<div class="six columns">
-					<label for="email">Email</label>
-					<input type="text" class="u-full-width" placeholder="xxxxx@yahoo.com" id="email" name="email">
-				</div>
-				<div class="six columns">
-					<label for="password">Password</label>
-					<input type="text" class="u-full-width" placeholder="Password" id="password" name="password">
-				</div>
-			</div>
-			<hr>
-			<input class="button-primary" type="submit" value="Submit" id="submit-button" data-role="none">
-  	  </div>
-  	</div>
-</form>
+				<input class="button-primary" type="submit" value="Create New Doctor" id="submit-button" data-role="none">
+			</form>
+		</div>
+	</div>
+
 
 <?php require 'templates/footer.php'; ?>
